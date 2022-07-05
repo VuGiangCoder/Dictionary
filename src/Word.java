7ec9386db02f662c398498ea3a0c67ebb54fc817
VuGiangCoder <giang2010gc@gmail.com>
2022-07-05 21:56:35 +0700

    Version 1.0

---
+++ src/Word.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Word {
+    private String word_target;  // từ mới
+    private String word_explain; // giải nghĩa
+
+
+}
+
+class Dictionary {
+
+
+}
+
+class DictionaryManagement {
+    public void insertFromCommandline(int word_count) {
+        Scanner sc= new Scanner(System.in);
+       Word[] word = new Word[word_count];
+
+    }
+}

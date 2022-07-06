dfe750db4edb5e816c8b766aa9d1465b9bc3c052
VuGiangCoder <giang2010gc@gmail.com>
2022-07-06 08:09:29 +0700

    Add Version1_0.java file

--- src/Version1_0.java
+++ src/Version1_0.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Version1_0 {
+import java.util.Scanner;
+
+public class Version1_0 {
+    public static void main(String[] args) {
+        DictionaryCommandline dictionaryCommandline=new DictionaryCommandline();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap so tu: ");
+        int word_count = sc.nextInt();
+        Word[] word = new Word[word_count];
+        dictionaryCommandline.dictionaryBasic(word_count,word);
+    }
 }
